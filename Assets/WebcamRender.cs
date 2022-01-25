4cdbd8c06bfae0c768c45f75a653c4bcf682f947
987123879113 <63495610+987123879113@users.noreply.github.com>
2022-01-26 08:54:31 +0900

    WIP DanceManiax cabinet

---
+++ Assets/WebcamRender.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WebcamRender : MonoBehaviour
+{
+    void Start()
+    {
+        var tex = new WebCamTexture("OBS-Camera");
+        tex.Play();
+
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+}
