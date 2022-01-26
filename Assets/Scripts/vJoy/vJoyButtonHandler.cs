af27b926e7369b8f65269a024f40fbe54fdf6ac6
987123879113 <63495610+987123879113@users.noreply.github.com>
2022-01-26 09:00:11 +0900

    Folder cleanup

---
+++ Assets/Scripts/vJoy/vJoyButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class vJoyButtonHandler : XRBaseInteractable
+{
+    private vJoyController JoystickController;
+    public int ButtonId;
+
+    public Material SensorOnMaterial;
+    public Material SensorOffMaterial;
+    public MeshRenderer SensorLight;
+
+    public void Start()
+    {
+        JoystickController = FindObjectOfType<vJoyController>();
+
+        if (SensorLight == null) {
+            SensorLight = GetComponent<MeshRenderer>();
+        }
+
+        SensorLight.material = SensorOffMaterial;
+    }
+
+    public void OnHoverEnterEvent(XRBaseInteractor interactor)
+    {
+        Debug.Log("Entered zone");
+        SensorLight.material = SensorOnMaterial;
+        JoystickController.SetButton(ButtonId, true);
+    }
+
+    public void OnHoverExitEvent(XRBaseInteractor interactor)
+    {
+        JoystickController.SetButton(ButtonId, false);
+        SensorLight.material = SensorOffMaterial;
+    }
+}
